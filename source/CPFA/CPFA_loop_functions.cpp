ee32c17c02f1bbf5e7f2866f48384726dd847180
artetrv <trevino.aeta@gmail.com>
2026-01-13 12:46:56 -0600

    files/random/clustered

--- source/CPFA/CPFA_loop_functions.cpp
+++ source/CPFA/CPFA_loop_functions.cpp
@@ -290,11 +290,11 @@ void CPFA_loop_functions::PostExperiment() {
             CollisionTime += c2.GetCollisionTime();
 		}
             
-			/*------------------------------------------------*/      
+			/*------------------------------------------------     
         {
             double collisions_sec = CollisionTime / (2.0 * ticks_per_second);
 
-            std::ofstream dataOutput("results/plot_experiments/Clustered_CPFA_r64_tag512_16by161.txt", std::ios::app);
+            std::ofstream dataOutput("results/plot_experiments/plots_random/Random_CPFA_r48_tag80_8by8.txt", std::ios::app);
 
             if(dataOutput.tellp() == 0) {
                 dataOutput << "score,time_seconds,collisions_seconds,seed\n";
